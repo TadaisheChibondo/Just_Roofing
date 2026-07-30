cd12b187b2139f084374eadf16b3b2460ae14537
TadaisheChibondo <n02428418x@students.nust.ac.zw>
2026-07-30 13:33:53 +0200

    added vercel url to cors

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -135,6 +135,7 @@ STATIC_URL = "static/"
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://just-roofing.vercel.app/"
 ]
 
 # Media Files (For Product and Portfolio Images)
